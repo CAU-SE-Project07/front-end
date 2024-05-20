ba049369ea2bc55d7629e94816d91b0624232212
2unhi <ironhak1106@gmail.com>
2024-05-20 16:58:59 +0900

    #2 [Modify] NavBar style [Add] Header Dropdown

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,27 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export const Navbar = () => {
+  const location = useLocation();
+
+  // 네비게이션 바 클릭 시 해당 페이지 파란색 버튼 생성
+  const getNavLinkClass = (path) => {
+    return location.pathname === path 
+      ? "text-white bg-gray-800 rounded-lg p-2" 
+      : "text-black hover:text-blue-500";
+  };
+
+  // 스타일 객체 생성
+  const activeLinkStyle = {
+    width: '160px',
+    textAlign: 'center'
+  };
+
   return (
-    <nav className="fixed left-0 top-16 h-full bg-gray-800 p-4 w-60"> {}
-      <div className="flex flex-col gap-4 text-white text-2xl mt-auto mb-4">
-        <Link to="/" className="hover:text-gray-300">Home</Link>
-        <Link to="/login" className="hover:text-gray-300">Login</Link>
-        <Link to="/signup" className="hover:text-gray-300">Signup</Link>
+    <nav className="fixed left-0 top-16 h-full w-60 border-r border-black flex flex-col justify-start pt-4">
+      <div className="flex flex-col gap-8 items-center">
+        <Link to="/" className={getNavLinkClass("/")} style={location.pathname === "/" ? activeLinkStyle : {}}>Home</Link>
+        <Link to="/login" className={getNavLinkClass("/login")} style={location.pathname === "/login" ? activeLinkStyle : {}}>Login</Link>
+        <Link to="/signup" className={getNavLinkClass("/signup")} style={location.pathname === "/signup" ? activeLinkStyle : {}}>Signup</Link>
       </div>
     </nav>
   );
